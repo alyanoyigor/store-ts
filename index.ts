b0754811000dfb2e03e70524e13a4f0731f92895
Igor Alyanoy <igoralyanoy@gmail.com>
2022-06-24 18:42:22 +0300

    feat: add full code after lecture

--- index.ts
+++ index.ts
@@ -3,58 +3,72 @@ interface IPerson {
 }
 
 enum Producer {
-  KyivBread = 'KyivBread',
-  Premiya = 'Premiya'
+  KyivKhlib = 'Kyiv Khlib Company',
+  Premia = 'Premia',
+  SvoyaLiniya = 'Svoya Liniya',
 }
 
-type IProduct = {
+type TProduct = {
   name: string;
   price: number;
   producer: Producer;
 };
 
-class Customer {
-  private productsCount = 0;
-  constructor(public name: string, private balance: number) {}
+const testProduct: TProduct = {
+  name: 'Morozyvo',
+  price: 15,
+  producer: Producer.SvoyaLiniya,
+};
 
-  public giveMoney(amount: number) {
-    if (this.balance <= amount) {
-      throw new Error('not enough money');
-    }
-    this.balance -= amount;
-  }
-
-  public takeProducts(productsCount: number) {
-    this.productsCount += productsCount;
-  }
+class Warehouse {
+  private products: TProduct[] = [];
 }
+
+class Cashier {}
+
+class WarehouseManager {}
 
 class Supermarket {
   readonly PRODUCT_PRICE = 10;
-  private productsCount = 100;
   private balance = 0;
-
+  private productsCount = 100;
   public sell(productCount: number, customer: Customer) {
     if (productCount > this.productsCount) {
-      throw new Error('Not enough products in market');
+      throw new Error(
+        `Not enough products in store, you are able to buy ${this.productsCount}`
+      );
     }
-
     const totalSum = productCount * this.PRODUCT_PRICE;
     this.takeMoneyFromCustomer(customer, totalSum);
     this.giveCustomerProducts(customer, productCount);
   }
-
   protected takeMoneyFromCustomer(customer: Customer, amount: number) {
     customer.giveMoney(amount);
     this.balance += amount;
   }
-
   protected giveCustomerProducts(customer: Customer, productsCount: number) {
     this.productsCount -= productsCount;
     customer.takeProducts(productsCount);
   }
 }
 
+class Customer {
+  private productsCount = 0;
+  constructor(public name: string, private balance: number) {}
+  public giveMoney(amount: number) {
+    if (this.balance >= amount) {
+      this.balance -= amount;
+    } else {
+      throw new Error('Not enough money...');
+    }
+  }
+  public takeProducts(productsCount: number) {
+    this.productsCount += productsCount;
+  }
+}
+
 const market = new Supermarket();
-const person = new Customer('Vova', 30);
-market.sell(2, person);
+const volodya = new Customer('Volodya', 30);
+market.sell(2, volodya);
+console.log(volodya);
+console.log(market);
